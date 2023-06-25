ba1ad1707518ce3766048c9546f1d9d14d311377
Ilia <ilya2491@gmail.com>
2023-06-25 18:18:35 +0300

    Add MARKs to viewController.

--- iOS-10-HW-13-Bessonov-Ilia/Sources/ViewController.swift
+++ iOS-10-HW-13-Bessonov-Ilia/Sources/ViewController.swift
@@ -9,10 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
+    // MARK: - Setup
+
+    // MARK: - Actions
 
 
 }
